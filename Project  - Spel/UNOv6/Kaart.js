7af4466b0b6d4a6e1ee4374a679b8d7d6133cfac
oBoii <fabian.denoodt@student.odisee.be>
2018-05-12 22:38:21 +0200

    backup waar ik verder mee ga

---
+++ Project  - Spel/UNOv6/Kaart.js
@@ -0,0 +1,28 @@
+class Kaart {
+  constructor(waarde, kleur, waardeOpScherm, idee) {
+    this.waarde = waarde;
+    this.kleur = kleur;
+    this.waardeOpScherm = waardeOpScherm;
+    this.idee = idee;
+  }
+
+  get getWaarde() {
+    return this.waarde;
+  }
+
+  setKleur(value) {
+    this.kleur = value;
+  }
+
+  get getKleur() {
+    return this.kleur;
+  }
+
+  get getIdee() {
+    return this.idee;
+  }
+
+  get getWaardeOpScherm() {
+    return this.waardeOpScherm;
+  }
+}
